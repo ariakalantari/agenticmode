cd38c2bb0e762734ccb7ca15777a8880e4d69681
Aria Kalantari <ariaxero@gmail.com>
2026-08-12 05:00:44 +0200

    Update Homebrew formula for v1.4.0

--- Formula/agenticmode.rb
+++ Formula/agenticmode.rb
@@ -4,17 +4,27 @@
 class Agenticmode < Formula
   desc "Keep a Mac awake with its lid closed while agent runs finish"
   homepage "https://github.com/ariakalantari/agenticmode"
-  url "https://github.com/ariakalantari/agenticmode/releases/download/v1.3.2/agenticmode.tar.gz"
-  sha256 "d80a96398a8f0b6f5381c037c2caa80c91b7991c6303e8e4d020d5c8483b1404"
+  url "https://github.com/ariakalantari/agenticmode/releases/download/v1.4.0/agenticmode.tar.gz"
+  sha256 "451a397f123bf916299ae739c501e56331f92a0ea8e3dd2f8c46571d54ac52d3"
   license "MIT"
   head "https://github.com/ariakalantari/agenticmode.git", branch: "main"
 
-  depends_on "go" => :build
+  depends_on "go" => :build if build.head?
   depends_on :macos
+
+  resource "ui" do
+    url "https://github.com/ariakalantari/agenticmode/releases/download/v1.4.0/agenticmode-ui"
+    sha256 "3e8bcc87b45d18670b121853c39399b2ef832dbc80853cb7810dffa190a28aaf"
+  end
 
   def install
     if build.head?
       system "go", "build", *std_go_args(output: libexec/"agenticmode-ui", ldflags: "-s -w"), "./cmd/agenticmode-ui"
+    else
+      resource("ui").stage do
+        chmod 0755, "agenticmode-ui"
+        libexec.install "agenticmode-ui"
+      end
     end
     bin.install "bin/agenticmode"
     bin.install_symlink bin/"agenticmode" => "am"
@@ -33,12 +43,10 @@ class Agenticmode < Formula
   end
 
   test do
-    assert_equal "agenticmode 1.3.2", shell_output("#{bin}/agenticmode --version").strip
-    assert_equal "agenticmode 1.3.2", shell_output("#{bin}/am --version").strip
+    assert_equal "agenticmode 1.4.0", shell_output("#{bin}/agenticmode --version").strip
+    assert_equal "agenticmode 1.4.0", shell_output("#{bin}/am --version").strip
     assert_match(/^  agenticmode \[options\]$/, shell_output("#{bin}/agenticmode --help"))
-    if build.head?
-      assert_predicate libexec/"agenticmode-ui", :executable?
-      assert_equal "1", shell_output("#{libexec}/agenticmode-ui --protocol-version").strip
-    end
+    assert_predicate libexec/"agenticmode-ui", :executable?
+    assert_equal "1", shell_output("#{libexec}/agenticmode-ui --protocol-version").strip
   end
 end
